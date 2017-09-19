6ba00da255564d3af8cd270d01dba174f6a4c858
Felipe Ramirez Mac <safe.ramirez@gmail.com>
2017-09-19 10:11:01 -0500

    Implement showing article details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,16 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 
 
 private
 
 def article_params
   params.require(:article).permit(:title, :body)
+end
+
 end
